89cb3ffc995c69ed5133e4a9b80e063552a2a931
carvesco <carvesco@gmail.com>
2025-08-01 15:01:48 -0400

    updated gh deployment

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,9 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:carvesco/PixeldailiesGallery.git main:gh-pages
 
 cd -
